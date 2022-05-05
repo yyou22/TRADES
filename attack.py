979e6c514db32e977422a5148d9d8a947bea0961
vyou <vyou@umich.edu>
2022-05-04 22:16:48 -0400

    start coding attack function

--- attack.py
+++ attack.py
@@ -10,8 +10,6 @@ import numpy as np
 from models.small_cnn import SmallCNN
 
 device = torch.device("cuda")
-model = SmallCNN().to(device)
-model.load_state_dict(torch.load('./checkpoints/model_mnist_smallcnn.pt'))
 
 #Maximum perturbation size for MNIST dataset must be smaller than 0.3
 epsilon = 0.3
@@ -26,4 +24,31 @@ def fgsm_attack(image, epsilon, data_grad):
 	# Return the perturbed image
 	return perturbed_image
 
+def attack(model, device, X_data, Y_data):
 
+	with torch.no_grad():
+		for idx in range(len(Y_data)):
+			# load original image
+            image = np.array(np.expand_dims(X_data[idx], axis=0), dtype=np.float32)
+            image = np.array(np.expand_dims(image, axis=0), dtype=np.float32)
+			# load label
+            label = np.array(Y_data[idx], dtype=np.int64)
+
+			# transform to torch.tensor
+            data = torch.from_numpy(image).to(device)
+            target = torch.from_numpy(label).to(device)
+
+			X, y = Variable(data, requires_grad=True), Variable(target)
+
+	return
+
+def main():
+	model = SmallCNN().to(device)
+	model.load_state_dict(torch.load('./checkpoints/model_mnist_smallcnn.pt'))
+
+	#load data
+	X_data = np.load('./data_attack/mnist_X.npy')
+	Y_data = np.load('./data_attack/mnist_Y.npy')
+
+if __name__ == '__main__':
+    main()
